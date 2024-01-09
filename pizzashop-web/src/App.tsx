2ad9015903bc3484a093700dae99c15c9e9de6d3
yagovela <yagovela@outlook.com>
2024-01-09 08:50:39 -0300

    New Project - Coffee Delivery, Design System, Github-Blog, Pizza Shop

---
+++ pizzashop-web/src/App.tsx
@@ -0,0 +1,19 @@
+import { Helmet, HelmetProvider } from 'react-helmet-async'
+import { RouterProvider } from 'react-router-dom'
+import { Toaster } from 'sonner'
+
+import { ThemeProvider } from './components/theme/theme-provider'
+import { router } from './pages/routes'
+
+export function App() {
+  return (
+    <HelmetProvider>
+      <ThemeProvider storageKey="pizzashop-theme" defaultTheme="system">
+        <Helmet titleTemplate="%s | pizza.shop" />
+        <Toaster richColors />
+
+        <RouterProvider router={router} />
+      </ThemeProvider>
+    </HelmetProvider>
+  )
+}
